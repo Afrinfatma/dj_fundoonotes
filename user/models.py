ce931de54a2ca165885dcde9ca1be297b6d82eac
Afrinfatma <afrinfatma4@gmail.com>
2022-09-23 16:13:13 +0530

    Added:custom and django authentication for login and registration

--- user/models.py
+++ user/models.py
@@ -1,12 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    user_name=models.CharField(max_length=255,unique=True)
-    password=models.CharField(max_length=255)
-    email=models.EmailField(max_length=255,unique=True)
-    phn_number=models.BigIntegerField()
+class User(AbstractUser):
+
+    phone_number=models.BigIntegerField()
     location=models.TextField(max_length=255)
 
     def __str__(self):
-        return self.user_name
+        return self.username
